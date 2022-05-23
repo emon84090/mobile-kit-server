8d5ba4a9dc46f8f6ecd8852f10ea7502ad03f686
emon84090 <emon84090@gmail.com>
2022-05-23 16:07:08 +0600

    admin remove and add done

--- index.js
+++ index.js
@@ -57,6 +57,10 @@ const run = async () => {
             res.send(result);
 
         })
+        app.get('/users', async (req, res) => {
+            const result = await mobilekitusers.find().toArray();
+            res.send(result)
+        })
 
         app.delete('/delete/:id', async (req, res) => {
             const id = req.params.id;
@@ -72,17 +76,62 @@ const run = async () => {
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
-            filter = { email: email };
+            const filter = { email: email };
             const options = { upsert: true };
             const updateDoc = {
                 $set: user,
             };
-
             const result = await mobilekitusers.updateOne(filter, updateDoc, options);
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, {
                 expiresIn: '1h'
             })
             res.send({ result, accesstoken: token });
+        })
+
+
+        app.put('/user/admin/:email', verifyjwt, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAcount = await mobilekitusers.findOne({ email: requester });
+
+            if (requesterAcount.role === "admin") {
+                const filter = { email: email };
+
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+
+                const result = await mobilekitusers.updateOne(filter, updateDoc);
+
+                res.send(result);
+            } else {
+                res.status(403).send({ message: "forbiden" })
+            }
+
+
+        })
+
+        app.put('/user/adminremove/:email', verifyjwt, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+
+            const requesterAcount = await mobilekitusers.findOne({ email: requester });
+
+            if (requesterAcount.role === "admin") {
+                const filter = { email: email };
+
+                const updateDoc = {
+                    $set: { role: 'user' },
+                };
+
+                const result = await mobilekitusers.updateOne(filter, updateDoc);
+
+                res.send(result);
+            } else {
+                res.status(403).send({ message: "forbiden" })
+            }
+
+
         })
 
 
